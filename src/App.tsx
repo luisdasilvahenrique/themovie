664a1a6e07fd631864d56b8b28cbe1b219dd943d
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-08-17 19:20:41 -0300

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header/Header"
+import './global.scss';
+
+function App() {
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import '../Header/styles.scss'
+import logoImg from '../../assets/logo.svg';
+import plusImg from '../../assets/plus.svg'
+export default function Header() {
+    return (
+        <>
+            <header className='container'>
+
+                <div className='content'>
+                    <img className='logo' src={logoImg} alt="logo" />
+
+                    <ul>
+                        <li>Filmes</li>
+                        <li>Séries</li>
+                        <li>Pessoas</li>
+                        <li>Mais</li>
+                    </ul>
+                </div>
+
+                <div className='primary'>
+                    <span>
+                        <img className="plus" src={plusImg} alt="plus" />
+                    </span>
+
+                    <div className="language">
+                       <a href="#">PT</a> 
+                    </div>
+                    <a href="#">Inicial Sessão</a>
+                    <a href="#">Aderir ao TMDB</a>
+                </div>
+            </header>
+        </>
+    );
+}
