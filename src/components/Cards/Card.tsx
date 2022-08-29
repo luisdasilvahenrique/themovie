587d40a772d362a33cc8b3b3577e607fd3652346
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-08-29 15:51:56 -0300

    circular-react-progress

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './styles.scss'
 import '../../global.scss';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 interface CardProps {
     id: number;
@@ -30,7 +32,40 @@ export default function Card() {
                     <div key={movie.id} className="card-content">
                         <img src={movie.image} alt="" />
                         <div className="info">
-                            <span>{movie.rate}%</span>
+                            
+                            <span>    
+                            <CircularProgressbar 
+                            className="progressbar" 
+                            value={movie.rate} 
+                            text={`${movie.rate}`}
+                            background={true}
+                            styles={{
+                                text: {
+                                  fill: '#fff',
+                                  fontSize: '48px',
+                                  fontWeight: '400', 
+                                
+                                },
+                                background: {
+                                  fill: 'rgb(3,37,65)',
+                                },
+                                path: {
+                                    // Path color
+                                    stroke: `rgb(1,210,119)`,
+                                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                                    strokeLinecap: 'butt',
+                                    // Customize transition animation
+                                    transition: 'stroke-dashoffset 0.5s ease 0s',
+                                    // Rotate the path
+                                    transform: 'rotate(0.25turn)',
+                                    transformOrigin: 'center center',
+                                  },
+                                
+                              }}
+
+                            />
+                            </span>
+                            
                             <h3>{movie.title}</h3>
                             <p>{movie.created_at}</p>
                         </div>
